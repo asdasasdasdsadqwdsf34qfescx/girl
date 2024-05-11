8a8c4147317f9b2acd915e2db7f85b51e25a3c04
Sergiu <sergiu.lungu@opentech.email>
2024-05-11 08:16:18 +0300

    first commit

---
+++ app/model/alala/page.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '20px' }}>
+  <iframe
+    width="640"
+    height="360"
+    frameBorder="0"
+    src="https://mega.nz/embed/6IEmUKAC#inLcDExRlo8a09pZIOjYVARdamHIKMgBIPkehhMjwsM"
+    allowFullScreen
+    style={{ maxWidth: '100%', marginBottom: '20px' }}
+  ></iframe>
+  <iframe
+    width="640"
+    height="360"
+    frameBorder="0"
+    src="https://mega.nz/embed/6IEmUKAC#inLcDExRlo8a09pZIOjYVARdamHIKMgBIPkehhMjwsM"
+    allowFullScreen
+    style={{ maxWidth: '100%', marginBottom: '20px' }}
+  ></iframe>
+   <iframe
+    width="640"
+    height="360"
+    frameBorder="0"
+    src="https://mega.nz/embed/6IEmUKAC#inLcDExRlo8a09pZIOjYVARdamHIKMgBIPkehhMjwsM"
+    allowFullScreen
+    style={{ maxWidth: '100%', marginBottom: '20px' }}
+  ></iframe>
+</div>
+
+  
+
+  );
+}
